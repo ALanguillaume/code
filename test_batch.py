dd247dc226c9d3714ba65ff266ceaeb420ab243a
ALanguillaume <antoinelanguillaume@gmail.com>
2025-02-04 15:41:30 +0100

    chore: split test suite

--- test_batch.py
+++ test_batch.py
@@ -1,11 +1,9 @@
-from datetime import date, timedelta
+from datetime import date
 import pytest
 
-from model import Batch, OrderLine, allocate
+from model import Batch, OrderLine
 
 today = date.today()
-tomorrow = today + timedelta(days=1)
-later = tomorrow + timedelta(days=10)
 
 
 def make_batch_and_line(sku, batch_qty, line_qty):
@@ -62,37 +60,3 @@ def test_allocation_is_idempotent():
     batch.allocate(line)
     batch.allocate(line)
     assert batch.available_quantity == 18
-
-
-def test_prefers_current_stock_batches_to_shipments():
-    in_stock_batch = Batch("in-stock-batch", "RETRO-CLOCK", 100, eta=None)
-    shipment_batch = Batch("shipment-batch", "RETRO-CLOCK", 100, eta=tomorrow)
-    line = OrderLine("oref", "RETRO-CLOCK", 10)
-
-    allocate(line, [in_stock_batch, shipment_batch])
-
-    assert in_stock_batch.available_quantity == 90
-    assert shipment_batch.available_quantity == 100
-
-
-def test_prefers_earlier_batches():
-    earliest = Batch("speedy-batch", "MINIMALIST-SPOON", 100, eta=today)
-    medium = Batch("normal-batch", "MINIMALIST-SPOON", 100, eta=tomorrow)
-    latest = Batch("slow-batch", "MINIMALIST-SPOON", 100, eta=later)
-    line = OrderLine("order1", "MINIMALIST-SPOON", 10)
-
-    allocate(line, [medium, earliest, latest])
-
-    assert earliest.available_quantity == 90
-    assert medium.available_quantity == 100
-    assert latest.available_quantity == 100
-
-
-def test_returns_allocated_batch_ref():
-    in_stock_batch = Batch("in-stock-batch-ref",
-                           "HIGHBROW-POSTER", 100, eta=None)
-    shipment_batch = Batch("shipment-batch-ref",
-                           "HIGHBROW-POSTER", 100, eta=tomorrow)
-    line = OrderLine("oref", "HIGHBROW-POSTER", 10)
-    allocation = allocate(line, [in_stock_batch, shipment_batch])
-    assert allocation == in_stock_batch.reference
