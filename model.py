f008eae859152109100714730537db565e7d1066
ALanguillaume <antoinelanguillaume@gmail.com>
2025-02-04 15:50:57 +0100

    feat: raise error if item of out stock when allocating

--- model.py
+++ model.py
@@ -56,7 +56,14 @@ class Batch:
         return self.available_quantity >= line.quantity and self.sku == line.sku
 
 
+class OutOfStock(Exception):
+    pass
+
+
 def allocate(line: OrderLine, batches: List[Batch]) -> str:
-    batch = next(b for b in sorted(batches) if b.can_allocate(line))
-    batch.allocate(line)
-    return batch.reference
+    try:
+        batch = next(b for b in sorted(batches) if b.can_allocate(line))
+        batch.allocate(line)
+        return batch.reference
+    except StopIteration:
+        raise OutOfStock(f"Out of stock for sku {line.sku}")
